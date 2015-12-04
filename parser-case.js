50bd148d463a12eedf767410ef26c34e2459b187
sol <darkbluever@gmail.com>
2015-12-04 11:37:04 +0800

    comment failed test case

--- parser-case.js
+++ parser-case.js
@@ -1,7 +1,7 @@
 QUnit.test('add',function(){
     var result = add(1,2)
     QUnit.ok(result === 3, '正整数加法')
-    QUnit.ok(result === -3)
-    QUnit.equal(result,-3)
+    // QUnit.ok(result === -3)
+    // QUnit.equal(result,-3)
     QUnit.strictEqual(result, 3)
 })
